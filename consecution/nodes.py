1ba7905d48f3d7531f48debdddb898b1ba8345f3
Rob deCarvalho <robdmc@gmail.com>
2015-12-12 22:28:57 -0500

    backup

--- consecution/nodes.py
+++ consecution/nodes.py
@@ -1,4 +1,6 @@
 import asyncio
+import time
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 
 class BaseNode:
     def __init__(self, name=''):
@@ -7,6 +9,8 @@ class BaseNode:
         self._queue = asyncio.Queue(2)
         self._loop = asyncio.get_event_loop()
         self.name = name
+        self.executor = ThreadPoolExecutor(max_workers=1)
+        #self.executor = ProcessPoolExecutor(max_workers=1)
 
     @property
     def downstream(self):
@@ -27,6 +31,7 @@ class BaseNode:
             other._downstream_nodes.extend([self])
 
     async def send(self, item):
+        print('{} send called with {}'.format(self.name, item))
         await self._queue.put(item)
 
     async def complete(self):
@@ -72,9 +77,9 @@ class ManualProducerNode(BaseNode):
 
 
 class ComputeNode(BaseNode):
-    def __init__(self, upstream=None, downstream=None, name='', sleep=None):
+    def __init__(self, upstream=None, downstream=None, name='', sleep_seconds=None):
         super(ComputeNode, self).__init__(name=name)
-        self.sleep = sleep
+        self.sleep_seconds = sleep_seconds
         if upstream:
             self.add_upstream_node(upstream)
         if downstream:
@@ -90,21 +95,44 @@ class ComputeNode(BaseNode):
                     self._queue.task_done()
                     break
                 else:
-                    await self.process(item)
+                    await self.process_runner(item)
                     self._queue.task_done()
             except RuntimeError:
                 raise
 
 
-    async def process(self, item):
-        # would really like this to not be async and actually called in 
-        # a separate thread (or process) so that it can have blocking code
-        nn = self.sleep if self.sleep else 0
-        print('  ' * int(10 * nn) + self.name, 'processing', item)
+    def push(self, item):
         if self.downstream:
-            if self.sleep:
-                await asyncio.sleep(self.sleep)
-            await self.downstream.send(item)
+            print('{} waiting till push completes'.format(self.name))
+            #self.
+            #self._loop.run_until_complete(self.downstream.send(item))
+            #await syncio.run_coroutine_threadsafe(self.downstream.send(item))
+            print('{} push done'.format(self.name))
+
+    async def process_runner(self, item):
+        print('{} starting process runner'.format(self.name))
+        await self._loop.run_in_executor(self.executor, self.process, item)
+
+
+
+
+    def process(self, item):
+
+        @self.in_parallel
+        def my_blocking_code(item):
+            time.sleep()
+
+        await asyncio.gather(all my block code)
+
+
+        #print('{} starting process'.format(self.name))
+        #nn = self.sleep_seconds if self.sleep_seconds else 0
+        #print('  ' * int(10 * nn) + self.name, 'processing', item)
+        #time.sleep(1)
+        #print('{} pushing'.format(self.name))
+        #self.push(item)
+        #print(' {} done pushing'.format(self.name))
+
 
 
 if __name__ == '__main__':
@@ -112,9 +140,7 @@ if __name__ == '__main__':
     n_comps = 2
     parent = producer
     for nn in range(n_comps):
-        parent = ComputeNode(upstream=parent, name='comp{:02d}'.format(nn + 1), sleep=.1 * nn)
-    #comp1 = ComputeNode(upstream=producer, name='comp1')
-    #comp2 = ComputeNode(upstream=comp1, name='comp2')
+        parent = ComputeNode(upstream=parent, name='comp{:02d}'.format(nn + 1), sleep_seconds=.1 * nn)
 
     producer.produce_from(range(3))
     master = asyncio.gather(*producer.get_starts())
@@ -123,60 +149,4 @@ if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     asyncio.ensure_future(master)
     loop.run_forever()
-
-
-#
-#
-#
-#
-#class PNode(ProcessorNode):
-#    def __init__(self, name, *args, **kwargs):
-#        super(PNode, self).__init__(*args, **kwargs)
-#        self.name = name
-#
-#    async def process(self):
-#        #print('starting p', self.upstream)
-#        async for item in self.upstream:
-#            out = ((self.name, item))
-#            await self.downstream.push(out)
-#
-#class FNode(ProcessorNode):
-#    def __init__(self, name, *args, **kwargs):
-#        super(FNode, self).__init__(*args, **kwargs)
-#        self.name = name
-#
-#    async def process(self):
-#        #print('starting f', self.upstream)
-#        async for item in self.upstream:
-#            #print('looping')
-#            out = ((self.name, item))
-#            print(out)
-#
-#
-#async def myfunc():
-#    return 'myret'
-#
-#async def main():
-#    gen = (nn for nn in range(3))
-#    producer = ProducerNode(gen)
-#    p1 = PNode(name='p1', upstream=producer)
-#    f2 = FNode(name='f2', upstream=p1)
-#    await producer.start()
-#    await p1.process()
-#    await f2.process()
-#
-#"""
-#I THINK I NEED TO DEFINE ANOTHER KIND OF NODE.  SO BASICALLY, I'LL HAVE
-#THREE KINDS OF NODES:  PRODUCER, PROCESSOR, AND CONSUMER.
-#PRODUCERS AND PROCESSORS JUST EXPOSE ITERATORS WHEREAS CONSUMERS LOOP
-#OVER THESE EXPOSED ITERATORS.
-#"""
-#
-#    #async for item in p1:
-#    #    print(item)
-#
-#
-#    #async for item in producer:
-#    #    print(('a', item))
-
 
