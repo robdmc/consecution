6f5b05f9eaab7af835d1738f594d871f5386a5e8
Rob deCarvalho <robdmc@gmail.com>
2015-12-28 20:20:10 -0500

    updating items play

--- consecution/items_play.py
+++ consecution/items_play.py
@@ -1,1 +1,26 @@
-import os
+def worker_func(item, state):
+    return item, state
+
+
+
+class Mapper(ProcessingNode):
+    async def process(self, item):
+        out_item = Item(anchor=item)
+        out_item.value = 1
+        await self.push(out_item)
+
+class Expander(ProcessingNode):
+    async def process(self, item):
+        out_list = []
+        for nn in range(10):
+            out_item = Item(anchor=item, value=1)
+            await self.push(out_item)
+
+class Reducer(ProcessingNode):
+    async def begin(self):
+        self.state.update(*args, **kwargs)
+    async def process(self, item):
+
+
+
+
