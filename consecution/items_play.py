7a9e4ea342d48dc705d8b7f3a58e3295e6fbc21d
Rob deCarvalho <robdmc@gmail.com>
2015-12-30 15:01:39 -0500

    working on acking

--- consecution/items_play.py
+++ consecution/items_play.py
@@ -1,4 +1,5 @@
 import random
+from collections import defaultdict
 
 class State:
     def __init__(self):
@@ -45,12 +46,19 @@ class Reducer(ProcessingNode):
         out_item = Item(anchor=self.multi_anchor, value=self.state.get('count'))
         self.push(out_item)
 
+
+##############################################################################
+##############################################################################
+##############################################################################
+##############################################################################
+
 class AckerEntry:
     def __init__(self, item):
-        self.hashed_keys = item.key
+        self.ack_hash = item.key
+        self.item = item
 
     def xor_item(self, item):
-        self.hashed_keys = self.hashed_keys ^ item.key
+        self.ack_hash = self.ack_hash ^ item.key
 
 
 class Acker:
@@ -58,25 +66,49 @@ class Acker:
         self._consucutor = consecutor
         self.entry_at_key = {}
         self.anchors_keys_for_item_key = {}
-        self.item_keys_for_anchor_key = {}
-
-
+        #self.item_keys_for_anchor_key = {}
 
     def register(self, item, anchors):
-        self.item_at_key.update({item.key: AckerEntry(item})
+        # create an entry for the newly registered item
+        self.entry_at_key[item.key] = AckerEntry(item)
+
+        # assemble a list of all anchor keys for this item
+        self.anchors_keys_for_item_key[item.key] = [
+            anchor.key for anchor in anchors
+        ]
+
+        # xor this item with each of its anchors
         for anchor in anchors:
-            self.item_at_key[anchor.key].xor_item(item)
+            self.entry_at_key[anchor.key].xor_item(item)
+
+    def ack(self, item):
+        # get the entry for this item, and xor it again for acking
+        entry = self.entry_at_key[item.key]
+        entry.xor_item(item)
+
+        # now ack this item for all its anchors
+        for anchor_key in self.anchors_keys_for_item_key[item.key]:
+            entry = self.entry_at_key[anchor_key]
+            entry.xor_item(item)
+            if entry.ack_hash == 0:
+                del self.entry_at_key[anchor_key]
+
+        # if entry is acked, delete it
+        if entry.ack_hash == 0:
+            del self.entry_at_key[item.key]
+            del self.anchors_keys_for_item_key[item.key]
+
 
 
 
 class Item:
-    def __init__(self, acker, anchor, value=None):
+    def __init__(self, acker, anchors, value=None):
         self.key = random.getrandbits(160)
         self.acker = acker
-        self.acker.register(self, anchor)
+        self.acker.register(self, anchors)
 
-    #def add_parerent(self, item):
-    #    self._parents.append(item)
+    def ack(self):
+        self.acker.ack(self)
 
 
 
