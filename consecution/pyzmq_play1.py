ac5c9b0179d0b16902d579a62e6b2d4c068b3aaa
Rob deCarvalho <robdmc@gmail.com>
2016-02-01 20:04:45 -0500

    starting to work on pyzmq version

--- consecution/pyzmq_play1.py
+++ consecution/pyzmq_play1.py
@@ -4,6 +4,36 @@ import zmq
 import multiprocessing
 import time
 
+SOCKET_FILE_PATH = '/tmp/consecutor'
+
+class IPCNameServer(object):
+    def __init__(self):
+        self.socket_file_path = SOCKET_FILE_PATH
+
+    def register_name(self, name):
+
+
+
+class Node(object):
+    def __init__(self, name, context=None, queue_size=None):
+        self.name = name
+        self.context = context if context else zmq.Context()
+        self.upstream_socket = self.context.socket(zmq.PULL)
+        self.downstream_socket = self.context.socket(zmq.PUSH)
+        self.consecutor_socket = self.context.socket(zmq.REQ)
+        if queue_size is not None:
+            self.upstream_socket.hwm = queue_size
+            self.downstream_socket = queue_size
+
+
+class Consecutor(object):
+    def __init__(self, name):
+        self.name = name
+
+
+
+
+
 PORT = 7777
 HWM = 1
 
