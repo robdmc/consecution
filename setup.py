03e4b69c7f287093794e1c5796ed178f168cfb78
Rob deCarvalho <robdmc@gmail.com>
2016-12-26 09:57:52 -0500

    flake8 fix

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read(path, encoding='utf-8'):
 
 def version(path):
     """Obtain the packge version from a python file e.g. pkg/__init__.py
-    See <https://packaging.python.org/en/latest/single_source_version.html>. 
+    See <https://packaging.python.org/en/latest/single_source_version.html>.
     """
     version_file = read(path)
     version_match = re.search(r"""^__version__ = ['"]([^'"]*)['"]""",
