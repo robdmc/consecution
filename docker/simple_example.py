301e5d9db4bcc98681576fe0f5192000a265898e
Rob deCarvalho <robdmc@gmail.com>
2016-12-26 10:00:13 -0500

    another flake8 fix

--- docker/simple_example.py
+++ docker/simple_example.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-#TODO: make the consecution install in the docker file read from pip
+# TODO: make the consecution install in the docker file read from pip
 from __future__ import print_function
 
 from consecution import Node, Pipeline
